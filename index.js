899573012c1a6ad87c29cc8b71fbab5c09f979a5
huska0707 <hussaink001haled@gmail.com>
2024-07-31 09:37:06 -0500

    get process list

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
-
+const { exec, execSync } = require("child_process");
 const app = express();
+const port = 3000;
 
-app.get("/process", (req, res) => {});
+app.get("/process", (req, res) => {
+  exec("tasklist", (err, stdout, stderr) => {
+    if (err) {
+      res.status(500).send(`Error: ${stderr}`);
+      return;
+    }
+    res.send(`<pre>${stdout}</pre>`);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Process control app listening at https://localhost:${port}`);
