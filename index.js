15b13286fe69ed529d3cc09cc568868f1cb5391c
huska0707 <hussaink001haled@gmail.com>
2024-07-31 09:40:18 -0500

    add process management endpoint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { exec, execSync } = require("child_process");
 const app = express();
 const port = 3000;
 
-app.get("/process", (req, res) => {
+app.get("/processes", (req, res) => {
   exec("tasklist", (err, stdout, stderr) => {
     if (err) {
       res.status(500).send(`Error: ${stderr}`);
@@ -13,6 +13,34 @@ app.get("/process", (req, res) => {
   });
 });
 
+app.get("/processes/:pid/:action", (req, res) => {
+  const { pid, action } = req.params;
+  let command;
+
+  switch (action) {
+    case "stop":
+      command = `taskkill /PID ${pid} /F`;
+      break;
+    case "start":
+      command = `start /B "your_program.exe"`;
+      break;
+    case "restart":
+      command = `taskkill /PID ${pid} /F && start /B "your_program.exe"`;
+      break;
+    default:
+      res.status(400).send("Invalid action");
+      return;
+  }
+
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      res.status(500).send(`Error: ${stderr}`);
+      return;
+    }
+    res.send(`Action '${action}' performed on process ${pid}.`);
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Process control app listening at https://localhost:${port}`);
+  console.log(`Process control app listening at http://localhost:${port}`);
 });
